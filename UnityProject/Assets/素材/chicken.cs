65e5c11524f91411a0c78e629dc46f53f1e2465a
as98as988 <57396803+as98as988@users.noreply.github.com>
2019-11-26 19:01:40 +0800

    chicken finish

---
+++ UnityProject/Assets/素材/chicken.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chicken : MonoBehaviour
+{
+    [Header("跳躍高度"), Range(50, 2000)]
+    public int jump = 100;
+    [Header("移動速度")]
+    public int speed = 100;
+    private void speak()
+    {
+        print("咕咕咕");
+
+    }
+    private void Start()
+    {
+        speak();
+    }
+    [Header("是否取得雞蛋")]
+    public bool egg = true;
+}
